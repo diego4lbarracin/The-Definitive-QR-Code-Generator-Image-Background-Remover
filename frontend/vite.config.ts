dde176722adfbc646787e09b90a84258dbfd21c8
diego4lbarracin <diegoalbarracin0405@gmail.com>
2026-01-15 13:48:28 -0500

    GitHub actions workflow for deployment.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/The-Definitive-QR-Code-Generator-Image-Background-Remover/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
